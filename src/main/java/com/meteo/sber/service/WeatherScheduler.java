15418eb5398f9fc7d1f5a624d03e486a8a2e3227
popkovvvv <popkovvvv@mail.ru>
2020-02-12 01:11:14 +0300

    add auto update weather, bud now dont now how to stop this process, and how start for one city not all..

--- src/main/java/com/meteo/sber/service/WeatherScheduler.java
+++ src/main/java/com/meteo/sber/service/WeatherScheduler.java
@@ -4,26 +4,14 @@ import com.meteo.sber.config.TaskSchedulerConf;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.task.TaskExecutorBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
-import org.springframework.scheduling.support.CronTrigger;
 import org.springframework.stereotype.Service;
-
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
-import java.util.TimeZone;
 import java.util.concurrent.ScheduledFuture;
 
-/**
- * Alternative version for DynamicScheduler
- * This one should support everything the basic dynamic scheduler does,
- * and on top of it, you can cancel and re-activate the scheduler.
- */
 @Service
 public class WeatherScheduler implements SchedulingConfigurer {
 
@@ -37,13 +25,11 @@ public class WeatherScheduler implements SchedulingConfigurer {
 
     TaskSchedulerConf taskSchedulerConf;
 
-
     public WeatherScheduler(@Autowired WeatherService weatherService, @Autowired TaskSchedulerConf taskSchedulerConf) {
         this.weatherService = weatherService;
+        this.taskSchedulerConf = taskSchedulerConf;
     }
 
-
-    // We can have multiple tasks inside the same registrar as we can see below.
     @Override
     public void configureTasks( ScheduledTaskRegistrar taskRegistrar) {
         if (scheduledTaskRegistrar == null) {
@@ -53,8 +39,7 @@ public class WeatherScheduler implements SchedulingConfigurer {
             taskRegistrar.setScheduler(taskSchedulerConf.poolScheduler());
         }
 
-
-        future = taskRegistrar.getScheduler().schedule(this::scheduleFixed, t -> {
+        future = taskRegistrar.getScheduler().schedule(this::schedule, t -> {
             Calendar nextExecutionTime = new GregorianCalendar();
             Date lastActualExecutionTime = t.lastActualExecutionTime();
             nextExecutionTime.setTime(lastActualExecutionTime != null ? lastActualExecutionTime : new Date());
@@ -63,7 +48,8 @@ public class WeatherScheduler implements SchedulingConfigurer {
         });
     }
 
-    public void scheduleFixed() {
+    public void schedule() {
+        weatherService.updateWeathers();
         LOGGER.info("scheduleFixed: Next execution time of this will always be 5 seconds");
     }
 
